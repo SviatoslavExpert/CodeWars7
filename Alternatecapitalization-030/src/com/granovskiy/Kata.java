3cf6d2bd0156440a493850c7cd77153fc6f76397
Sviatoslav <s.granovskiy@gmail.com>
2019-10-21 19:31:11 +0300

    Initial commit

---
+++ Alternatecapitalization-030/src/com/granovskiy/Kata.java
@@ -0,0 +1,34 @@
+package com.granovskiy;
+
+import java.util.Arrays;
+
+public class Kata {
+    public static String[] capitalize(String s){
+        String[] charsOne = s.split("");
+        String[] charsTwo = s.split("");
+        char temp = ' ';
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                temp = Character.toUpperCase(s.charAt(i));
+                charsOne[i] = charsOne[i].replace(s.charAt(i), temp);
+            } else {
+                temp = Character.toUpperCase(s.charAt(i));
+                charsTwo[i] = charsTwo[i].replace(s.charAt(i), temp);
+            }
+        }
+        String delimiter = "";
+        String result1 = String.join(delimiter, charsOne);
+        String result2 = String.join(delimiter, charsTwo);
+        //String[] result = new String[] {result1, result2};
+        return new String[] {result1, result2};
+    }
+
+    public static void main(String[] args) {
+        capitalize("abcdef");
+    }
+}
+
+//String result1 = Arrays.toString(charsOne);
+//String result2 = Arrays.toString(charsTwo);
+//System.out.println(Arrays.toString(charsOne) + " " + Arrays.toString(charsTwo));
+        //System.out.println(Arrays.toString(result));
