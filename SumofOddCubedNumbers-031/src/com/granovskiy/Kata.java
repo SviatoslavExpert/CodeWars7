7cef4f4a774d2c66b95204d909d27d61c9c24e90
Sviatoslav <s.granovskiy@gmail.com>
2019-10-22 10:14:08 +0300

    Initial commit

---
+++ SumofOddCubedNumbers-031/src/com/granovskiy/Kata.java
@@ -0,0 +1,14 @@
+package com.granovskiy;
+
+public class Kata {
+    public static int cubeOdd(int arr[]) {
+        int sum = 0;
+        for (int i : arr) {
+            int cube = i * i * i;
+            if (cube % 2 != 0)
+                sum += cube;
+        }
+        return sum;
+    }
+}
+//   Done!!!
