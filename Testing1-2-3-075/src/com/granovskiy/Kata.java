2f7d5f33f29ed51ba6b530ff405a15fcf3f6ae24
Sviatoslav <s.granovskiy@gmail.com>
2019-11-11 22:00:41 +0200

    Initial commit

---
+++ Testing1-2-3-075/src/com/granovskiy/Kata.java
@@ -0,0 +1,24 @@
+package com.granovskiy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Kata {
+    public static List<String> number(List<String> lines) {
+        List<String> linesUpdated = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < lines.size(); i++) {
+            builder.append(i + 1 + ": ");
+            builder.append(lines.get(i));
+            lines.set(i, builder.toString());
+            //builder.delete(i, i + 1);
+        }
+        //Arrays.toString(lines);
+        return lines;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(number(Arrays.asList("a", "b", "c")));
+    }
+}
